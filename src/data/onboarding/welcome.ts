11eb2cc0286a8979564f8cee01bebc3e3868e06c
jezuchryste <33784443+jezuchryste@users.noreply.github.com>
2025-04-30 20:11:25 +0200

    zmiany

--- src/data/onboarding/welcome.ts
+++ src/data/onboarding/welcome.ts
@@ -7,17 +7,17 @@ export interface HowItWorksStep {
 export const howItWorksSteps: HowItWorksStep[] = [
     {
         number: 1,
-        title: "Recenzuj Firmy",
-        description: "Dziel się swoimi doświadczeniami i zdobywaj punkty za recenzje"
+        title: "Dziel się feedbackiem",
+        description: "Przeglądaj firmy uczestniczące w naszym programie, zbieraj swoje sugestie, pomysły oraz wątpliwości a następnie przekaż je w formie feedbacku do właściciela biznesu"
     },
     {
         number: 2,
-        title: "Zweryfikuj swoją firmę",
-        description: "Zdobądź weryfikację, aby zbierać i zarządzać opiniami klientów"
+        title: "Punkty za feedback",
+        description: "Każdorazowo pozostawiony feedback, który zostanie zaakceptowany przez drugą stronę wiąże się z otrzymaniem symbolicznego 0,5 punkta za przesłany feedback"
     },
     {
         number: 3,
-        title: "Buduj reputację",
-        description: "Zdobądź zaufanie klientów dzięki autentycznym opiniom i rekomendacjom"
+        title: "Wymiana punktów",
+        description: "Zdobyte punkty umożliwiają Tobie zobieranie feedbacku o swojej firmie."
     }
 ]; 
